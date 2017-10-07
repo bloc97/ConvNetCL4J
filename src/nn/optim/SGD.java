ce75dba457e9c62acc951aaa33ba1d4b52af63d1
bloc97 <bloc97@users.noreply.github.com>
2017-10-07 12:33:20 -0400

    completed SGD

--- src/nn/optim/SGD.java
+++ src/nn/optim/SGD.java
@@ -5,10 +5,26 @@
  */
 package nn.optim;
 
+import nn.Layer;
+import nn.Network;
+import nn.NeuronLayer;
+import nn.optim.kernels.SGDKernel;
+
 /**
  *
  * @author bowen
  */
 public class SGD {
+    
+    public final static SGDKernel KERNEL = new SGDKernel();
+    
+    public void update(Network network, float learningRate) {
+        for (Layer layer : network.getLayers()) {
+            if (layer instanceof NeuronLayer) {
+                NeuronLayer nlayer = (NeuronLayer) layer;
+                KERNEL.call(nlayer.getWeights(), nlayer.getGradients(), learningRate);
+            }
+        }
+    }
     
 }
